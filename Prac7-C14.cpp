39f44b5ae800787bd57a76d25349922fd310b4f6
Anshul Singh <anshulsingh024@yahoo.com>
2024-05-02 20:53:24 +0530

    optimized Adj Matrix Code

--- Prac7-C14.cpp
+++ Prac7-C14.cpp
@@ -5,100 +5,47 @@ The node can be represented by the airport name or name of the city. Use adjacen
 */
 
 #include <iostream>
-#include <queue>
 using namespace std;
-
-int adj_mat[50][50] = {0, 0};
-int visited[50] = {0};
-
-void dfs(int s, int n, string arr[])
-{
-    visited[s] = 1;
-    cout << arr[s] << " ";
-    for (int i = 0; i < n; i++)
-    {
-        if (adj_mat[s][i] && !visited[i])
-            dfs(i, n, arr);
-    }
-}
-
-void bfs(int s, int n, string arr[])
-{
-    bool visited[n];
-    for (int i = 0; i < n; i++)
-        visited[i] = false;
-    int v;
-    queue<int> bfsq;
-    if (!visited[s])
-    {
-        cout << arr[s] << " ";
-        bfsq.push(s);
-        visited[s] = true;
-        while (!bfsq.empty())
-        {
-            v = bfsq.front();
-            for (int i = 0; i < n; i++)
-            {
-                if (adj_mat[v][i] && !visited[i])
-                {
-                    cout << arr[i] << " ";
-                    visited[i] = true;
-                    bfsq.push(i);
-                }
-            }
-            bfsq.pop();
-        }
-    }
-}
 
 int main()
 {
-	cout<<"DSAL Practical No. 07 (C-14)"<<endl;
-	cout<<"Prepared By : Anshul Singh"<<endl;
-    
-    cout << "\nEnter no. of cities: ";
-    int n, u;
-    cin >> n;
+    int n;
+    cout<<"Enter No. of cities";
+    cin>>n;
     string cities[n];
-    for (int i = 0; i < n; i++)
+    int adj[n][n] = {0,0};
+    for(int i = 0; i<n; i++)
     {
-        cout << "Enter city #" << i << " (Airport Code): ";
-        cin >> cities[i];
+        cout<<"Enter City Name "<<i<<endl;
+        cin>>cities[i];
     }
-    
-    cout << "\nYour cities are: " << endl;
-    for (int i = 0; i < n; i++)
-        cout << "city #" << i << ": " << cities[i] << endl;
-    
-    for (int i = 0; i < n; i++)
+
+    for(int i = 0; i<n; i++)
     {
-        for (int j = i + 1; j < n; j++)
+        for(int j = i+1; j<n; j++)
         {
-            cout << "Enter distance between " << cities[i] << " and " << cities[j] << " : ";
-            cin >> adj_mat[i][j];
-            adj_mat[j][i] = adj_mat[i][j];
+            cout<<"Enter Distance Between "<<cities[i]<<" and "<<cities[j]<<endl;
+            cin>>adj[i][j];
+            adj[j][i] = adj[i][j];
         }
     }
-    
+
     //display
-    cout << endl;
-    for (int i = 0; i < n; i++)
-        cout << "\t" << cities[i] << "\t";
+    cout<<endl;
+    for(auto i : cities)
+    {
+        cout<<"\t"<<i<<"\t";
+    }
+    cout<<endl;
     for (int i = 0; i < n; i++)
     {
-        cout << "\n"
-             << cities[i];
+        cout<<cities[i]<<"\t";
         for (int j = 0; j < n; j++)
-            cout << "\t" << adj_mat[i][j] << "\t";
-        cout << endl;
+        {
+            cout<<"\t"<<adj[i][j]<<"\t";
+        }
+        cout<<endl;
     }
-    cout << "Enter Starting Vertex For DFS : ";
-    cin >> u;
-    cout << "DFS: ";
-    dfs(u, n, cities);
-    cout <<"\nEnter Starting Vertex For BFS : ";
-    cin >> u;
-    cout << "BFS: ";
-    bfs(u, n, cities);
+    
     return 0;
 }
